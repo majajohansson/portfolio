dd0d7d1427d53734a4b02d087527eefbdb4e42ea
BrunoGodefroy <godefroy.bruno77@gmail.com>
2018-06-22 08:24:01 +0200

    Use browser router instead of hash router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { Home, AboutMe, MoankArticle } from './pages';
 import scrollUpOnEnter from './hoc/scrollUpOnEnter';
 
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <Switch>
       <Route exact path="/" component={scrollUpOnEnter(Home)} />
       <Route exact path="/about-me" component={scrollUpOnEnter(AboutMe)} />
       <Route path="/article/moank" component={scrollUpOnEnter(MoankArticle)} />
     </Switch>
-  </HashRouter>,
+  </BrowserRouter>,
   window.document.getElementById('app'),
 );
