020eba20da881f6d0bcf52ec0954e5389a2e732d
BrunoGodefroy <godefroy.bruno77@gmail.com>
2019-01-20 12:53:55 +0100

    Update design of banner

--- src/styledComponents/StyledBanner.js
+++ src/styledComponents/StyledBanner.js
@@ -3,18 +3,21 @@ import { media } from './styles';
 import StyledParagraph from './StyledParagraph';
 
 const StyledBanner = styled.div`
-  margin: 9% auto;
-  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 3rem auto;
+  width: 70%;
 
   > img {
     border-radius: 50%;
-    display: inline-block;
-    width: 15%;
-    vertical-align: middle;
+    width: 6rem;
   }
 
   ${media.phone`
-    display: none;
+    flex-wrap: wrap;
+    width: 100%;
+    text-align: center;
   `}
 `;
 
